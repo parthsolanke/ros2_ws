2a1eb0199244d0c887ae6eeff2a352b882c19fea
parthsolanke <parthsolanke2002@gmail.com>
2023-05-21 22:34:58 +0530

    added build folder

---
+++ build/trial_tutorial_pkg/share/trial_tutorial_pkg/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/mnt/c/Users/Parth Solanke/Documents/dev/ros2_ws/build/trial_tutorial_pkg"
